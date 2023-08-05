bc13a955a8227a6108ed068e53042228397c9513
Abdulsalam Umar <salamtura@gmail.com>
2023-08-06 00:11:33 +0100

    updates #deploy

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Payment extends Model
@@ -25,8 +26,8 @@ class Payment extends Model
         'fee',
     ];
 
-    public function invoice(): HasMany
+    public function invoice(): BelongsTo
     {
-        return $this->hasMany(Invoice::class);
+        return $this->belongsTo(Invoice::class);
     }
 }
