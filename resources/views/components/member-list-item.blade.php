2ee1f366f4bb29903f8be38a5e4aaa62d8d8eed4
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-12-26 00:30:48 +0100

    Feature: Implement Member List on Dashboard

---
+++ resources/views/components/member-list-item.blade.php
@@ -0,0 +1,11 @@
+<div style="display: flex; gap: 50px; justify-content: space-between; align-items: center;">
+    <div style="display: flex; gap: 10px; align-items: center; width: 250px;">
+        <div class="polo-member-pic" style="background-image: url('{{ $image }}')"></div>
+        <div>
+            <div style="color: #18181B; font-size: 14px; font-weight: 700">{{ $name }}</div>
+            <div style="color: #71717A; font-size: 13px; font-weight: 400">{{ $email }}</div>
+        </div>
+    </div>
+    <div style="color: #908E8F; font-size: 13px; font-weight: 500">{{ $points }}</div>
+    <div style="color: #374140; font-size: 14px; font-weight: 500">View</div>
+</div>
