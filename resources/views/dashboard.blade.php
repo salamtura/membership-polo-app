641c79a782b1b2b4b47f36c11b0dcab1c554bf5b
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-12-28 09:37:35 +0100

    Revert "Refactor: Create Profile Restriction Card Component"
    This reverts commit 3849859e426a0464abddba0ffe3f69bb1faeac82.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -99,10 +99,33 @@
             @if ($invoices->count() > 0 || $penalties->count() > 0 || $user->membership->subscription_status != 'active')
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <x-profile-card :name="$user->name" :year="substr($user->membership->created_at, 0, 4)" :status="$user->membership->membership_status" />
-                    <x-profile-restriction-card />
+                    <div class="bg-red-100 border-t-4 border-red-500 rounded-b text-teal-900 px-4 py-3 shadow-md "
+                        role="alert">
+                        <div class="flex">
+                            <div class="py-1"><svg class="fill-current h-6 w-6 text-red-500 mr-4"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path
+                                        d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                                </svg></div>
+                            <div>
+                                <p class="font-bold">Profile Restricted</p>
+                                <p class="text-sm mt-2">
+                                    Dear Member,<br><br>
+                                    Your profile has been restricted due to unpaid invoices or penalties
+                                    associated
+                                    with your profile.
+                                    Therefor you are unable to view pitch status or book for chukkers.
+                                    Kindly make payment for any outstanding invoices to unrestrict your
+                                    profile.<br><br>
+                                    Thank you for understanding.<br>
+                                    Management.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             @else
-                <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
                     <x-profile-card :name="$user->name" :year="substr($user->membership->created_at, 0, 4)" :status="$user->membership->membership_status" />
                     @if (\Carbon\Carbon::now()->greaterThan($pitch->to_date))
                         @php $pitch->status = 'closed'; @endphp
@@ -174,7 +197,7 @@
                     </div>
                 </div>
 
-                <!-- Old Dashboard Code Hidden-->
+                <!-- Old Dashboard Code -->
                 <div class="container mx-auto p-5 relative hidden">
                     <div class="md:flex no-wrap md:-mx-2 ">
                         <!-- Left Side -->
