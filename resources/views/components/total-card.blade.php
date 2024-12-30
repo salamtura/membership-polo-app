cce24eed95dc4f60e38f8adb0568edbb590309c5
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-12-30 09:02:02 +0100

    Style: Correct Total Cards

--- resources/views/components/total-card.blade.php
+++ resources/views/components/total-card.blade.php
@@ -1,6 +1,6 @@
 <div class="common-card flex flex-col gap-[30px]">
-    <div class="flex flex-grow">
-        <div class="flex-grow font-[500] text-[12px]  sm:text-[16px]">
+    <div class="flex gap-2 flex-grow">
+        <div class="flex-grow font-[500] text-[14px]  sm:text-[16px]">
             <?php echo htmlspecialchars($name); ?>
         </div>
         <img class="w-[24px] h-[24px] sm:w-auto sm:h-auto" src="{{ $icon }}" />
@@ -9,7 +9,7 @@
         <div class="text-[30px] font-[500]">
             <?php echo number_format($value); ?>
         </div>
-        <div class="pill-grey text-[10px] sm:text-[16px] self-start sm:self-auto">
+        <div class="pill-grey text-[11px] sm:text-[16px] self-start sm:self-auto" style="text-wrap-mode: nowrap;">
             <?php echo htmlspecialchars($status); ?>
         </div>
     </div>
