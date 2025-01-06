6c12214ef08841267dbbbabaccb81a4613e07f2d
Sadiq Umar <umarsadiqibrahim@gmail.com>
2025-01-06 12:17:37 +0100

    style: redesign club fees page

---
+++ resources/views/components/club-fee-card.blade.php
@@ -0,0 +1,16 @@
+<div class="common-card flex flex-col gap-4">
+    <div class="flex">
+        <div class="flex flex-col gap-2">
+            <p class="text-lg font-bold">{{ $name }}</p>
+            {{-- <p class="text-md">{{ $description }}</p> --}}
+        </div>
+        {{-- <div>
+            <div class="status-pill">{{ $scope }}</div>
+        </div> --}}
+    </div>
+    <hr />
+    <div class="flex flex-col gap-2">
+        <p class="text-4xl text-[#394911] font-bold">{{ $price }}</p>
+        <p>only</p>
+    </div>
+</div>
